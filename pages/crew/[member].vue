ccaf25503a9598e51d02a163492691199a99ad61
Ben Hong <ben@bencodezen.io>
2023-08-29 12:13:24 -0700

    feature: user can see specific page for crew member

---
+++ pages/crew/[member].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const memberName = computed(() => route.params.member)
+</script>
+
+<template>
+  <h1>{{ memberName }} Page</h1>
+</template>
+
+<style lang="scss" scoped></style>
