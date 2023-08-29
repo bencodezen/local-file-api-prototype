f5e9a6aba7f197c0d7e9c7876393eb45188f4e7b
Ben Hong <ben@bencodezen.io>
2023-08-29 12:13:14 -0700

    refactor: create composable to centralize file system api usage

---
+++ composables/useFileSystemAPI.ts
@@ -0,0 +1,47 @@
+export const useFileSystemAPI = () => {
+  const directoryHandle = ref(null)
+
+  async function createFile(memberName) {
+    const newFileHandle = await directoryHandle.value.getFileHandle(
+      `${memberName}.json`,
+      { create: true }
+    )
+
+    return newFileHandle
+  }
+
+  async function getDirectoryHandle() {
+    directoryHandle.value = await window.showDirectoryPicker()
+  }
+
+  async function readFiles() {
+    const promises = []
+
+    for await (const entry of directoryHandle.value.values()) {
+      if (entry.kind !== 'file') {
+        continue
+      }
+      promises.push(
+        entry.getFile().then(file => {
+          return file.text()
+        })
+      )
+    }
+
+    return await Promise.all(promises)
+  }
+
+  async function writeFile(fileHandle, contents) {
+    const writable = await fileHandle.createWritable()
+    await writable.write(contents)
+    await writable.close()
+  }
+
+  return {
+    directoryHandle,
+    createFile,
+    getDirectoryHandle,
+    readFiles,
+    writeFile
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { useFileSystemAPI } from '../composables/useFileSystemAPI'
+
+const { directoryHandle, getDirectoryHandle, readFiles, writeFile } =
+  useFileSystemAPI()
+
+const crew = ref([])
+const newMember = ref('')
+
+async function addMember() {
+  crew.value.push(newMember.value)
+  const fileHandle = await createFile(newMember.value)
+  await writeFile(fileHandle, JSON.stringify({ name: newMember.value }))
+  newMember.value = ''
+}
+
+async function deleteMember(member) {
+  crew.value = crew.value.filter(m => m !== member)
+  // TODO: Refactor this to a utility function
+  await directoryHandle.value.removeEntry(`${member}.json`)
+}
+
+// TODO: Update member info
+async function selectDirectory() {
+  await getDirectoryHandle()
+  // Read each file's content
+  const files = await readFiles()
+  // Push it into the app's data
+  files.forEach(file => {
+    const { name } = JSON.parse(file)
+    crew.value.push(name)
+  })
+}
+</script>
+
+<template>
+  <div>
+    <h1>One Piece Crew</h1>
+    <input type="text" v-model="newMember" @keyup.enter="addMember" />
+
+    <ul>
+      <li v-for="(member, index) in crew" :key="`${member}-${index}`">
+        <NuxtLink :to="`/crew/${member}`">{{ member }}</NuxtLink> -
+        <button @click="deleteMember(member)">🗑️ Delete</button>
+      </li>
+    </ul>
+
+    <h2>Local File Settings</h2>
+
+    <button @click="selectDirectory">Get Directory</button>
+    <p>Directory Handle: {{ directoryHandle }}</p>
+  </div>
+</template>
